89105030f8c478478c53b3485f5bec512156d296
carovg7 <carovg7@gmail.com>
2020-11-11 11:49:47 -0700

    Se agregó la función print_protein_and_stop_codon_using_standard_table() que a partir de una secuencia de ADN, imprime su mRNA, proteínas y stop codon (dentro de un diccionario). [biopython-tarea2-ex3]

--- Script.py
+++ Script.py
@@ -32,9 +32,39 @@ if __name__=="__main__":
 #///////////////////////////////////////////////////////////////////////
 from Bio.Seq import Seq
 
+secuencia1 = Seq("GATCGATGGGCCTATATAGGATCGAAAATCGC")
+secuencia2 = Seq("GACGTACCTATGTATAGCGATACGTTAGCTAC")
+
 def concatenate_and_get_reverse_of_complement(secuencia1, secuencia2):
-    secuencia1 = Seq("GATCGATGGGCCTATATAGGATCGAAAATCGC")
-    secuencia2 = Seq("GACGTACCTATGTATAGCGATACGTTAGCTAC")
     secuencia = secuencia1 + secuencia2
-    print(secuencia.reverse_complement())
+    rev_com = secuencia.reverse_complement()
+    return rev_com
+    print(rev_com)
 concatenate_and_get_reverse_of_complement(secuencia1, secuencia2)
+
+#/////////////////////////////////////////////////////////////////////
+from Bio.Seq import Seq
+
+cadena = "ATGTCACTTACTTACTCACAGTCTTAA"
+def print_protein_and_stop_codon_using_standard_table(cadena):
+    secuencia = Seq(cadena)
+    diccionario = {}
+    mrna = secuencia.transcribe()
+    diccionario['mRNA'] = mrna
+    for i in range(len(secuencia)):
+            if((secuencia[i*3:i*3+3] == 'ATG') or (secuencia[i*3:i*3+3] == 'TTG') or (secuencia[i*3:i*3+3] == 'CTG')):
+                proteins = secuencia[i*3:].translate(to_stop = True)
+                diccionario['Proteins'] = proteins
+
+                for j in range(len(secuencia)):
+                    if((secuencia[j*3:j*3+3] == 'TAG') or (secuencia[j*3:j*3+3] == 'TAA') or (secuencia[j*3:j*3+3] == 'TGA')):
+                        diccionario['Stop codon'] = secuencia[j*3:j*3+3]
+                        break
+
+            if(i+1 == len(secuencia)):
+                break
+    return diccionario
+r = print_protein_and_stop_codon_using_standard_table(cadena)
+print(r)
+
+#//////////////////////////////////////////////////////////////////
